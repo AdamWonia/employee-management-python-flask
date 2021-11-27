2bb6ccbe86f7ac29ad521114ba638e06de9eba4c
Adam Wonia <adam.wonia@gmail.com>
2021-11-27 14:59:16 +0100

    created model Employee and connected with mysql database

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:adam_root@localhost/employee_management'
+app.secret_key = "MySecretKey###321"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:adam_root@localhost:3306/employ_manage'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
