da97fb6c79243039c6bc0c48607b6d16c8fe91dc
Adam Wonia <adam.wonia@gmail.com>
2021-11-27 16:27:15 +0100

    base template added

--- my_app/employee_management/views.py
+++ my_app/employee_management/views.py
@@ -3,25 +3,6 @@ from flask import render_template, request, flash, redirect, url_for
 from my_app.employee_management.models import Employee
 
 
-@app.route('/')
+@app.route('/home')
 def index():
     return render_template('index.html')
-
-
-@app.route('/insert', methods=['POST'])
-def insert():
-    if request.method == 'POST':
-        name = request.form['name']
-        surname = request.form['surname']
-        email = request.form['email']
-        phone = request.form['phone']
-        position = request.form['position']
-        data = Employee(name, surname, email, phone, position)
-        db.session.add(data)
-        db.session.commit()
-        flash("Employee added successfully!")
-
-        return redirect(url_for('index'))
-
-    else:
-        return "test"
