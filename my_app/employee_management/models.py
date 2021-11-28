e50b2dea17fb24b773e6c06cb4467b3d755b67be
Adam Wonia <adam.wonia@gmail.com>
2021-11-28 21:14:24 +0100

    log in, register and log out functionalities tested

--- my_app/employee_management/models.py
+++ my_app/employee_management/models.py
@@ -27,7 +27,7 @@ class Employee(db.Model):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
     def __init__(self, username, password):
         self.username = username
--- my_app/employee_management/views.py
+++ my_app/employee_management/views.py
@@ -1,6 +1,18 @@
-from my_app import app, db
+from my_app import app, db, bcrypt
 from flask import render_template, request, flash, redirect, url_for
-from my_app.employee_management.models import Employee, LoginForm, RegisterForm
+from my_app.employee_management.models import Employee, User, LoginForm, RegisterForm
+from flask_login import login_user, LoginManager, login_required, logout_user, current_user
+
+# new lines
+# add login_required decorator
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(id)
 
 
 @app.route('/home', methods=['GET'])
@@ -63,11 +75,31 @@ def delete(id=None):
 @app.route('/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('index'))
+
     return render_template('login.html', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-    
+    if form.validate_on_submit():
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hash_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
     return render_template('register.html', form=form)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
